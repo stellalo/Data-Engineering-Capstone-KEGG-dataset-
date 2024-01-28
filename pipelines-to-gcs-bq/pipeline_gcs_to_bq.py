d2b0592bad1b43c1b5809fa65981f9810cb383fe
stellalo <89308696+stellalo@users.noreply.github.com>
2024-01-28 15:57:13 -0500

    Update pipeline_gcs_to_bq.py

--- pipelines-to-gcs-bq/pipeline_gcs_to_bq.py
+++ pipelines-to-gcs-bq/pipeline_gcs_to_bq.py
@@ -4,36 +4,28 @@ from prefect import flow,task
 #from prefect_gcp.cloud_storage import GcsBucket 
 from prefect_gcp import GcpCredentials 
 #from google.cloud import storage
-import os
-
-@task
-def get_files(folder_path):
-    files = os.listdir(folder_path)
-    #filter out '.DS_Store'
-    files = [file for file in files if not file.startswith('.')]
-    return files
 
 
-# def list_files_in_folder(bucket, folder):
-#     # Initialize the GCS client
-#     storage_client = storage.Client()
-#     # Get the bucket
-#     bucket = storage_client.get_bucket(bucket)
-#     # List files under the specified folder
-#     blobs = bucket.list_blobs(prefix=folder)
-#     # Extract file names
-#     file_list = [blob.name for blob in blobs if blob.name.endswith('/') == False]
-#     #print(file_list)
-#     return file_list
-# @task(retries=3)
-# def extract_from_gcs(gcs_path:str) -> Path:
-#     """Download trip data from gcs"""
-#     #load an instance of a class named GcsBucket 
-#     gcs_block = GcsBucket.load("kegg-gcs")
-#     local_path=f"/Users/stella/Desktop/kegg-orchestration/{gcs_path}.parquet"
-#     #write file to local
-#     gcs_block.get_directory(from_path=gcs_path, local_path=local_path)
-#     return local_path
+def list_files_in_folder(bucket, folder):
+    # Initialize the GCS client
+    storage_client = storage.Client()
+    # Get the bucket
+    bucket = storage_client.get_bucket(bucket)
+    # List files under the specified folder
+    blobs = bucket.list_blobs(prefix=folder)
+    # Extract file names
+    file_list = [blob.name for blob in blobs if blob.name.endswith('/') == False]
+    #print(file_list)
+    return file_list
+@task(retries=3)
+def extract_from_gcs(gcs_path:str) -> Path:
+    """Download trip data from gcs"""
+    #load an instance of a class named GcsBucket 
+    gcs_block = GcsBucket.load("kegg-gcs")
+    local_path=f"kegg-orchestration/{gcs_path}.parquet"
+    #write file to local
+    gcs_block.get_directory(from_path=gcs_path, local_path=local_path)
+    return local_path
 
 @task(retries=3)
 def read_file(path: Path) -> pd.DataFrame:
@@ -62,8 +54,7 @@ def write_bq(df:pd.DataFrame,table:str,type:str) -> None:
 def to_bq(files:list[str],folder_path:str,type:str):
     """Main flow to load data into big query data warehouse"""
     for file in files:
-        #path = extract_from_gcs(file)
-        path = f'{folder_path}/{file}'
+        path = extract_from_gcs(file)
         table_name = parse_path(file)
         df = read_file(path)
         write_bq(df,table_name,type)
@@ -71,9 +62,8 @@ def to_bq(files:list[str],folder_path:str,type:str):
 @flow()
 def parent_flow(file_type:list[str]):
     for type in file_type:
-        folder_path= f'/Users/stella/Desktop/kegg-orchestration/data/{type}'
-        #files = get_files(bucket, folder)
-        files = get_files(folder_path)
+        folder_path= f'kegg-orchestration/data/{type}'
+        files = get_files(bucket, folder)
         to_bq(files,folder_path,type)
 
 if __name__ == '__main__':
