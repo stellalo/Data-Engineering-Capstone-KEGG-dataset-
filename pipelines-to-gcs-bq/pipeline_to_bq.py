4813014be4804ab1418e3a976dc577e75d9f37c8
stellalo <89308696+stellalo@users.noreply.github.com>
2024-01-28 15:57:39 -0500

    Update pipeline_to_bq.py

--- pipelines-to-gcs-bq/pipeline_to_bq.py
+++ pipelines-to-gcs-bq/pipeline_to_bq.py
@@ -13,28 +13,6 @@ def get_files(folder_path):
     files = [file for file in files if not file.startswith('.')]
     return files
 
-
-# def list_files_in_folder(bucket, folder):
-#     # Initialize the GCS client
-#     storage_client = storage.Client()
-#     # Get the bucket
-#     bucket = storage_client.get_bucket(bucket)
-#     # List files under the specified folder
-#     blobs = bucket.list_blobs(prefix=folder)
-#     # Extract file names
-#     file_list = [blob.name for blob in blobs if blob.name.endswith('/') == False]
-#     #print(file_list)
-#     return file_list
-# @task(retries=3)
-# def extract_from_gcs(gcs_path:str) -> Path:
-#     """Download trip data from gcs"""
-#     #load an instance of a class named GcsBucket 
-#     gcs_block = GcsBucket.load("kegg-gcs")
-#     local_path=f"kegg-orchestration/{gcs_path}.parquet"
-#     #write file to local
-#     gcs_block.get_directory(from_path=gcs_path, local_path=local_path)
-#     return local_path
-
 @task(retries=3)
 def read_file(path: Path) -> pd.DataFrame:
     df = pd.read_parquet(path)
@@ -62,7 +40,6 @@ def write_bq(df:pd.DataFrame,table:str,type:str) -> None:
 def to_bq(files:list[str],folder_path:str,type:str):
     """Main flow to load data into big query data warehouse"""
     for file in files:
-        #path = extract_from_gcs(file)
         path = f'{folder_path}/{file}'
         table_name = parse_path(file)
         df = read_file(path)
@@ -72,7 +49,6 @@ def to_bq(files:list[str],folder_path:str,type:str):
 def parent_flow(file_type:list[str]):
     for type in file_type:
         folder_path= f'kegg-orchestration/data/{type}'
-        #files = get_files(bucket, folder)
         files = get_files(folder_path)
         to_bq(files,folder_path,type)
 
